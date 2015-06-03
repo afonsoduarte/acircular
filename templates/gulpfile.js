1bde980fd400c262e6bb20a0a67889f803493c2e
Afonso Duarte <afonsomartins@gmail.com>
2015-06-03 17:24:45 +0100

    basket style

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -8,12 +8,12 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function () {
     return gulp.src('scss/style.scss')
-        .pipe($.sourcemaps.init())
+        // .pipe($.sourcemaps.init())
             .pipe($.sass({errLogToConsole: true}))
             .pipe($.autoprefixer('last 2 versions'))
-        .pipe($.sourcemaps.write('./maps'))
+        // .pipe($.sourcemaps.write('./maps'))
         .pipe(gulp.dest('css'))
-        //.pipe(browserSync.stream())
+        .pipe(browserSync.stream())
         .pipe($.size())
         .pipe($.notify("Compilation complete."));
 });
@@ -41,7 +41,7 @@ gulp.task('serve', function () {
 gulp.task('watch', ['serve'], function () {
  
     // watch for changes
-    //gulp.watch('**/*.html').on('change', browserSync.reload);
+    gulp.watch('**/*.html').on('change', browserSync.reload);
     gulp.watch('scss/**/*.scss', ['styles']);
-    //gulp.watch('js/**/*.js', ['scripts']);
+    gulp.watch('js/**/*.js', ['scripts']);
 });
